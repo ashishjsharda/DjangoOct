58180afa0a63f32dc4b88dd221ad3433d04dd170
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-18 15:25:56 -0400

    Add all my files

--- posts/models.py
+++ posts/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     url=models.URLField()
-    poster=models.ForeignKey(User)
+    poster=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
 # Create your models here.
