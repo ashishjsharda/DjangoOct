e5136c92b358a15592226b77415aff10fde60e66
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-23 11:58:04 -0400

    Add all my files

--- posts/models.py
+++ posts/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     url=models.URLField()
     poster=models.ForeignKey(User,on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
+    created=models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering=['-created']
+        ordering=["-created"]
 
 class Vote(models.Model):
     voter=models.ForeignKey(User,on_delete=models.CASCADE)
