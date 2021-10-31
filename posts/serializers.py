d31227e2e93649e833c30825d778bbe530b64cfb
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-31 05:22:48 -0400

    Add all my files

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,3 +2,10 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields=['id','title','url','poster','created']
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
