a2149a5f7cc1be9514d5830bee244a3c33d62b7f
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-01 05:18:34 -0400

    Update urls

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,7 +5,4 @@ class PostSerializers(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields=['id','title','url','poster','created']
-    title = models.CharField(max_length=100)
-    url = models.URLField()
-    poster = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
+
