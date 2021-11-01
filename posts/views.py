1d19545e5926d11664341c7af6527085245a9518
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-01 05:21:23 -0400

    Update views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Post
 # Create your views here.
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
