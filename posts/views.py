00aa041e53ff7c3596a9dec46faa5c1df7195241
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-21 08:22:30 -0500

    Modify to ListsCreateAPIView

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,6 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializers
 # Create your views here.
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
