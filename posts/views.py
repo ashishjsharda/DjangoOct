1d7f4a24657a18b2e993ea6918e68d32c78069e1
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-28 15:31:24 -0500

    Add permission

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import Post
 from .serializers import PostSerializers
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
+    permission_classes=[permissions.IsAuthenticated]
 
     def perform_create(self,serializer):
         serializer.save(poster=self.request.user)
