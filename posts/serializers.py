53a66c6de4fdd19a966bc737bf84eecda6a680d4
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-27 19:43:23 -0500

    Add id

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,6 +4,7 @@ from .models import Post
 
 class PostSerializers(serializers.ModelSerializer):
     poster=serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.username')
     class Meta:
         model = Post
         fields=['id','title','url','poster','created']
